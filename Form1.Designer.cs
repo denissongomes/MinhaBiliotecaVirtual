ecba1c269d75d9994e3591dfc45f70e59a6ae040
Denisson Gomes <denissongomes@outlook.com.br>
2024-08-11 18:40:32 -0300

    Add  frmLogin  and frmRegistro

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,16 +1,16 @@
 ﻿namespace MinhaBiliotecaVirtual
 {
-    partial class Form1
+    partial class frmRegistro
     {
         /// <summary>
-        /// Required designer variable.
+        /// Variável de designer necessária.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        /// Limpar os recursos que estão sendo usados.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,29 +20,206 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Código gerado pelo Windows Form Designer
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsuatio = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtConfSenha = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBxMostrarSenha = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // Form1
+            // label1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
+            this.label1.Location = new System.Drawing.Point(22, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Registro";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Usuário";
+            // 
+            // txtUsuatio
+            // 
+            this.txtUsuatio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
+            this.txtUsuatio.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUsuatio.Font = new System.Drawing.Font("MS Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuatio.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtUsuatio.Location = new System.Drawing.Point(27, 130);
+            this.txtUsuatio.Multiline = true;
+            this.txtUsuatio.Name = "txtUsuatio";
+            this.txtUsuatio.Size = new System.Drawing.Size(216, 28);
+            this.txtUsuatio.TabIndex = 2;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
+            this.txtSenha.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSenha.Font = new System.Drawing.Font("MS Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenha.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtSenha.Location = new System.Drawing.Point(27, 192);
+            this.txtSenha.Multiline = true;
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(216, 28);
+            this.txtSenha.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Senha";
+            // 
+            // txtConfSenha
+            // 
+            this.txtConfSenha.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
+            this.txtConfSenha.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtConfSenha.Font = new System.Drawing.Font("MS Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConfSenha.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtConfSenha.Location = new System.Drawing.Point(27, 255);
+            this.txtConfSenha.Multiline = true;
+            this.txtConfSenha.Name = "txtConfSenha";
+            this.txtConfSenha.Size = new System.Drawing.Size(216, 28);
+            this.txtConfSenha.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 23);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Confirmar senha";
+            // 
+            // checkBxMostrarSenha
+            // 
+            this.checkBxMostrarSenha.AutoSize = true;
+            this.checkBxMostrarSenha.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkBxMostrarSenha.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBxMostrarSenha.Location = new System.Drawing.Point(104, 288);
+            this.checkBxMostrarSenha.Name = "checkBxMostrarSenha";
+            this.checkBxMostrarSenha.Size = new System.Drawing.Size(141, 27);
+            this.checkBxMostrarSenha.TabIndex = 7;
+            this.checkBxMostrarSenha.Text = "Mostrar senha";
+            this.checkBxMostrarSenha.UseVisualStyleBackColor = true;
+            this.checkBxMostrarSenha.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(32, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 35);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "REGISTRAR";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
+            this.button2.Location = new System.Drawing.Point(32, 376);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(216, 35);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "LIMPAR";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(64, 432);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(164, 23);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Já está registrado? ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
+            this.label6.Location = new System.Drawing.Point(112, 464);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 23);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Entre";
+            // 
+            // frmRegistro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(285, 544);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBxMostrarSenha);
+            this.Controls.Add(this.txtConfSenha);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUsuatio);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(165)))), ((int)(((byte)(169)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmRegistro";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsuatio;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtConfSenha;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBxMostrarSenha;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
