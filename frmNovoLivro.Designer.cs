5f8e8a2bb16efeb888f15e50fb9a0364638f0af4
Denisson Gomes <denissongomes@outlook.com.br>
2024-08-14 13:45:59 -0300

    Designate forms  (frmNovo and frmEditar) button as the cancel button.

--- frmNovoLivro.Designer.cs
+++ frmNovoLivro.Designer.cs
@@ -146,6 +146,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancelar;
             this.ClientSize = new System.Drawing.Size(406, 339);
             this.Controls.Add(this.btnOK);
             this.Controls.Add(this.btnCancelar);
