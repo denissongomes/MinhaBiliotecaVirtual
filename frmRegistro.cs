a48ba5c1242106608cddbb7acf8afcd389b92616
Denisson Gomes <denissongomes@outlook.com.br>
2024-08-15 17:19:57 -0300

    Add logic event to  button btnRegister_Click()

--- frmRegistro.cs
+++ frmRegistro.cs
@@ -32,20 +32,27 @@ namespace MinhaBiliotecaVirtual
 
         }
 
-        void Connect()
-        {
-           
-        }
-
         private void btnRegister_Click(object sender, EventArgs e)
         {
             if (txtUsuatio.Text == "" &&  txtUsuatio.Text == "" && txtConfSenha.Text == "")
             {
                 MessageBox.Show("Os campos Usuário e Senha estão vazios!", "Falha no Registro", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            else if (txtSenha.Text == txtConfSenha.Text){ 
-            
+            else if (txtSenha.Text == txtConfSenha.Text){
 
+
+                con.Open();
+                string registroSQL = "INSERT INTO Usuários VALUES ('"+txtUsuatio.Text+"', '"+txtSenha.Text+"')";
+                MySqlCommand cmd = new MySqlCommand(registroSQL, con);
+                MySqlDataReader dr = cmd.ExecuteReader();
+                con.Close();
+                MessageBox.Show("Sua conta foi criada com sucesso!", "Confirmação de registro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            } else
+            {
+                MessageBox.Show("A senha não corresponde, por favor digite novamente.", "Falha no Registro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtSenha.Text = "";
+                txtConfSenha.Text = "";
+                txtSenha.Focus();
             }
         }
     }
